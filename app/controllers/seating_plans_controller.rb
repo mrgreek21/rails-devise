cb15cc3e149815a34dabfd92d1d2cc9a5d2eb3ac
Andreas Kyprianou <mr_greek21@hotmail.com>
2016-08-13 20:57:37 -0400

    allow user to say how many tables are part of the seating plan, and create those tables in the database

--- app/controllers/seating_plans_controller.rb
+++ app/controllers/seating_plans_controller.rb
@@ -10,6 +10,8 @@ class SeatingPlansController < ApplicationController
   def create
   	@seating_plan = SeatingPlan.new(seating_plan_params)
     if @seating_plan.save!
+      params[:num_tables].to_i.times {Table.create(:seating_plan => @seating_plan)}
+
       redirect_to :controller => 'events', :action => 'show', :id => seating_plan_params[:event_id]
     else
       redirect_to :controller => 'seating_plans', :action => 'new'
--- app/models/seating_plan.rb
+++ app/models/seating_plan.rb
@@ -1,6 +1,10 @@
 class SeatingPlan < ActiveRecord::Base
-	belongs_to :event
-	has_many :tables
+  belongs_to :event
+  has_many :tables
 
-	validates :event, presence: true
+  validates :event, presence: true
+
+  def num_tables
+    Table.where(:seating_plan => self).count
+  end
 end
